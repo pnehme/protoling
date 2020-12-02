4b51556f530cb6d05e36533364cf26d84fe0b4d5
Paulo <paulo@tsat.com.br>
2020-12-02 09:50:53 -0300

    02-12-2020

--- venv/norma_xyz.py
+++ venv/norma_xyz.py
@@ -12,15 +12,13 @@ def comparaAmplitude(x, minimo, maximo):
     passo = passo / 40
     for i in range(1,41):
         if minimo + passo * (i - 1) <= x < minimo + passo * i:
-           valor = i/40
+           valor = i/41
     return valor
 
 target = np.identity(59,dtype=float)
 indice = -1
 connection = acc.exec_con()
 
-cabecalho = ['nest_id','treatment','r_time','a_x','a_y','a_z']
-file = open("c:/BASE1/NNTW/norma_cluster_faixas.csv", "w")
 for i in range(1,16):
     if i < 10:
         ninho = 'N0'+str(i)+'YCC2017'
@@ -33,20 +31,36 @@ for i in range(1,16):
         population = []
         for rows in df.itertuples():
             population.append([mt.sqrt(rows.a_x**2 + rows.a_y**2 + rows.a_z**2),rows.r_time])
+        X_3sigma = np.array(population)
+        plt.clf()
+        plt.title(ninho+"-"+tratamento)
+        plt.xlabel('tempo(acelerômetro)')
+        plt.ylabel('norma(x,y,z)')
+        plt.plot(X_3sigma[:,1], X_3sigma[:,0])
+#        plt.show()
+        plt.savefig("C:/Ciencia_dados_PUC/Redes neurais/analise_protoling/normas_vetores/"+ninho+"-"+tratamento+".jpg")
+
+'''
+        file = open("c:/BASE1/NNTW/normas"+ninho+"-"+tratamento+".csv", "w")
+        dados = 'norma_vetor,r_time\n'
         X = np.array(population)
         media = X[:,0].mean()
         sigma = X[:,0].std()
         minimo = X[:,0].min()
         maximo = X[:,0].max()
-#        print(ninho, tratamento, format(media, '.2f'), format(sigma,'.2f'), format(minimo, '.2f'), format(maximo, '.2f'))
-
-        dados = ""
-        passo = 0
+        for i in range(len(population)):
+            dados += str(format(population[i][0], '.2f')) + ',' + str(population[i][1])+ '\n'
+        print(ninho, tratamento)
+        file.write(dados)
+        file.close()
+        dados = ''
+'''
+'''
         for i in range(len(population)):
             if population[i][0] > media + 10*sigma:
                 if len(population) - i >= 512:
                     for k in range(0,511):
-                        dados += str(comparaAmplitude(population[k+i][0], minimo, maximo)) + ","
+                        dados += str(comparaAmplitude(population[k+i][0], minimo, maximo)) + "," 
                     for id in range(0,59):
                         dados += str(target[indice][id]) + ','
                     dados += '\n'
@@ -57,8 +71,10 @@ for i in range(1,16):
                     print(ninho, tratamento, str(passo))
                 if passo == 3:
                     break
-
 '''
+'''
+# usa para verificação dos clusters 
+                    for id in range(0,59):
                         dados += str(format(population[k+i][0], ".2f"))+","+str(k)+'\n'
                     i += 512
                     file.write(dados)
@@ -70,9 +86,5 @@ for i in range(1,16):
 '''
 
 
-file.close()
-#        X_3sigma = np.array(dados)
-#        plt.xlabel('tempo(segundos)')
-#        plt.ylabel('norma(x,y,z) > 3sigma')
-#        plt.plot(X_3sigma[:,1], X_3sigma[:,0])
-#        plt.show()
+#file.close()
+
--- venv/teste_rede.py
+++ venv/teste_rede.py
@@ -1,28 +1,46 @@
 from sklearn.neural_network import MLPClassifier
-#from sklearn import datasets
+import math as mt
 import pandas as pd
-#import numpy as np
+from pg_db import acc_db as acc
 import csv
-'''
-data = {}
-with open("c:/BASE1/NNTW/norma.csv") as arquivocsv:
-    ler = csv.DictReader(arquivocsv, delimiter=",")
-    for linhas in ler:
-        print(linhas)
 
+def comparaAmplitude(x, minimo, maximo):
+    valor = 0
+    passo = 0
+    passo = maximo - minimo
+    passo = passo / 40
+    for i in range(1,41):
+        if minimo + passo * (i - 1) <= x < minimo + passo * i:
+           valor = i/40
+    return valor
 
-vals = np.fromiter(ler.values(), dtype=float)
-print(vals)
-'''
+connection = acc.exec_con()
+
 norma = pd.read_csv('c:/BASE1/NNTW/norma_cluster_faixas.csv')
 entradas = norma.iloc[0:171,0:510].copy()
 saidas = norma.iloc[0:171,511:570].copy()
 
-
-redeneural = MLPClassifier(verbose=True,
+redeneural = MLPClassifier(verbose=False,
                            max_iter=100000,
                            tol=0.00001,
                            activation='logistic',
                            learning_rate_init=0.001)
 redeneural.n_outputs_= 59
 redeneural.fit(entradas,saidas)
+ninho = 'N01YCC2017'
+tratamento = 'T01'
+df = pd.read_sql_query(
+    "select a_x, a_y, a_z, r_time from data_vibration_1 where nest_id = '" + ninho + "' and treatment = '" + tratamento + "' order by r_time",
+    connection)
+population = []
+i = 0
+for rows in df.itertuples():
+    population.append(mt.sqrt(rows.a_x ** 2 + rows.a_y ** 2 + rows.a_z ** 2))
+minimo = min(population)
+maximo = max(population)
+
+for n in range(len(population)):
+    population[n] = comparaAmplitude(population[n], minimo, maximo)
+
+X
+
